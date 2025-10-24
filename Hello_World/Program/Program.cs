212985c2a6c838c81b037323bfb2e59a83e7031f
Mateusz <herdzina.mateuszdev@gmail.com>
2025-10-24 10:11:59 +0200

    First practice

---
+++ Hello_World/Program/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Int32 number1 = 542;
+        Console.WriteLine(number1);
+        int zmienna_sparsowana = Int32.Parse(args[0]);
+        Console.WriteLine(zmienna_sparsowana);
+    }   
+}
