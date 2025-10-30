c34072908324221cdbaa5fdae1b129820a4c7392
Mateusz <herdzina.mateuszdev@gmail.com>
2025-10-30 08:16:15 +0100

    Drugie cwiczenie

---
+++ Hello_World_2/Program_2/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program_2
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World 2!");
+        Int32 number = 42;
+        Int32 number2 = Int32.Parse(args[0]);
+        if (number < number2)
+        {
+            Console.WriteLine("Napisaliśmy pierwszego ifa!");
+        }
+    }
+}
