289cda0c3f3ab9a7e8567516d8b1b678d9125083
Mateusz <herdzina.mateuszdev@gmail.com>
2025-11-06 14:58:43 +0100

    Kolejne cwiczenie

---
+++ Hello_World_3/kolejny_program/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static private void Main(string[] args)
+    {
+
+        Int32 zmienna1 = 1;
+        if (zmienna1 > 5)
+        {
+            Console.WriteLine("Prawda");
+        }
+        else
+        {
+            Console.WriteLine("Fałsz");
+        }
+    }
+}
